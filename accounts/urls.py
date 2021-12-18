dd7be3eb20eab7e6287d305b08ef0a5835495657
Susmita-Pal <sushmitasp302@gmail.com>
2021-12-18 02:03:17 -0500

    removed the user routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,13 +4,13 @@ from accounts import views
 urlpatterns = [
     path('user-register/',views.userRegister,name='userRegister'),
 
-    path('user-login/', views.userLogin, name='userLogin'),
+    #path('user-login/', views.userLogin, name='userLogin'),
     path('admin-login/', views.adminLogin, name='adminLogin'),
-    path('forgot-password/',views.forgotPassword, name='forgotPassword'),
+    #path('forgot-password/',views.forgotPassword, name='forgotPassword'),
     path('forgot-password-admin/', views.forgotPasswordAdmin, name='forgotPasswordAdmin'),
-    path('user-logout/', views.userLogout, name='userLogout'),
+    #path('user-logout/', views.userLogout, name='userLogout'),
     path('admin-logout/', views.adminLogout, name='adminLogout'),
-    path('user-dashboard/',views.dashboardUser,name='dashboardUser'),
+   # path('user-dashboard/',views.dashboardUser,name='dashboardUser'),
     path('admin-dashboard/',views.dashboardAdmin,name='dashboardAdmin'),
     path('admin-dashboard/school-view/', views.dashboardAdminSchoolView, name='dashboardAdminSchoolView'),
     path('admin-dashboard/individual-view/',views.dashboardAdminIndividualView,name='dashboardAdminIndividualView')
